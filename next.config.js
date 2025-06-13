4e19894ec5543f202d188aa948515e78d6abbbe4
helloteacoo <helloteacoo@gmail.com>
2025-06-13 10:59:41 +0800

    移除多選題的設計與開發，完成單選題的AI轉換

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@ag-grid-community/core'],
   output: 'standalone',
   images: {
     unoptimized: true
