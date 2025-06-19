f36d497c605b1aa1775c12232814c34ec924dc13
helloteacoo <helloteacoo@gmail.com>
2025-06-20 00:38:48 +0800

    主要功能全部完成

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,14 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 /// <reference types="next/navigation-types/compat/navigation" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    [elemName: string]: any;
+  }
+}
